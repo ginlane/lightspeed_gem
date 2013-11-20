99b60b77678f7d24dc06e81fabaa41d0c1d31854
Denis Ivanov <visible@jumph4x.net>
2013-11-20 17:03:45 -0500

    Ignoring logout request

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -3,4 +3,8 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr_cassettes'
   c.hook_into :webmock
+
+   c.ignore_request do |request|
+    URI(request.uri).path.match 'sessions/current/logout'
+  end
 end
